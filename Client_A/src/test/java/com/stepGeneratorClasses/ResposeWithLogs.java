4c9152f933b6a4fc85360f5c6897a8c3371ce13e
Kunal <Kunal@Kunal>
2021-03-10 10:25:49 +0530

    Assignment3

--- Client_A/src/test/java/com/stepGeneratorClasses/ResposeWithLogs.java
+++ Client_A/src/test/java/com/stepGeneratorClasses/ResposeWithLogs.java
@@ -1,5 +1,7 @@
 package com.stepGeneratorClasses;
 
+import org.testng.Assert;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -11,17 +13,21 @@ public class ResposeWithLogs {
 	@Given("Send Request to the link with log")
 	public void send_Request_to_the_link_with_log() {
 	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("\nLog when sending request\n");
 		response=RestAssured.given().when().log().all().get("https://api.ratesapi.io/api/latest?base=USD");
 	}
 
 	@When("Response is received with log")
 	public void response_is_received_with_log() {
+		System.out.println("\nLog after receiving response\n");
+		RestAssured.get("https://api.ratesapi.io/api/latest?base=USD").then().log().all();
 	    // Write code here that turns the phrase above into concrete actions
 	    
 	}
 
 	@Then("Validate the response with log")
 	public void validate_the_response_with_log() {
+		Assert.assertEquals(200, response.getStatusCode(),"Response Received");
 	    // Write code here that turns the phrase above into concrete actions
 	}
 
